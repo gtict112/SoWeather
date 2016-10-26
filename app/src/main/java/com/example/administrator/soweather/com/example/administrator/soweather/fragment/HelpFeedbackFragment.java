62b900c1445e342c0b71e6a79b64a038a382c876
lihailin <li14786865847haili@qq.com(935361900)>
2016-10-26 18:37:57 +0800

    commit(提交信息)

--- app/src/main/java/com/example/administrator/soweather/com/example/administrator/soweather/fragment/HelpFeedbackFragment.java
+++ app/src/main/java/com/example/administrator/soweather/com/example/administrator/soweather/fragment/HelpFeedbackFragment.java
@@ -1,18 +1,23 @@
 package com.example.administrator.soweather.com.example.administrator.soweather.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.example.administrator.soweather.R;
+import com.example.administrator.soweather.com.example.administrator.soweather.activity.HelpFeedbackActivity;
 
 /**
  * Created by Administrator on 2016/10/26.
  */
 
-public class HelpFeedbackFragment extends Fragment {
+public class HelpFeedbackFragment extends Fragment implements View.OnClickListener {
+    private RelativeLayout feed_back;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +30,25 @@ public class HelpFeedbackFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_time, null);
-        Bundle bundle = getArguments();
+        View view = inflater.inflate(R.layout.fragment_help_feedback, null);
+        initView(view);
         return view;
     }
 
+    private void initView(View view) {
+        feed_back = (RelativeLayout) view.findViewById(R.id.feed_back);
+        feed_back.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.feed_back:
+                Intent intent = new Intent(getActivity(), HelpFeedbackActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
 }
